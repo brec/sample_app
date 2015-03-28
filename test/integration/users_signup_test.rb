0b15152110fcc0335f3dd211d4585c47fcc348b8
Brandon <renoz.82@gmail.com>
2015-03-28 05:02:36 -0500

    Add enhancements to error reporting and UI

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,7 +11,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 									 password_confirmation: "bar"}
 		end
 		assert_template 'users/new'
+		assert_select 'div#error_explanation'
+		assert_select 'div.field_with_errors'
 	end
+
 
 	test "valid signup information" do
 		get signup_path
@@ -22,5 +25,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 												  password_confirmation: "password"}
 		end
 		assert_template 'users/show'
+		assert_not flash.empty?
 	end		
 end
